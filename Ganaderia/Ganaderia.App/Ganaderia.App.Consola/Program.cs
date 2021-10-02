2f7e58f1628433299a7d6869eb17691098a1d895
LuisMendoza <luismendoza3110@gmail.com>
2021-10-01 23:58:25 -0500

    V1.0.0 Creación inicial del proyecto

---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Consola/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
+
+
+namespace Ganaderia.App.Consola
+{
+    class Program
+    {
+        private static IRepositorioGanadero _repositorioGanadero = new RepositorioGanadero(new Persistencia.AppContext());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            AddGanadero();
+        }
+
+        private static void AddGanadero()
+        {
+         var ganadero = new Ganadero
+         {
+          Nombres = "Sergio",
+          Apellidos = "Moreno",
+          NumeroTelefono = "3045956788",
+          Correo = "luismendoza3110@gmail.com",
+          Contrasena = "123456",
+          Latitude = 4554,
+          Longitude = 5454
+         };
+          _repositorioGanadero.AddGanadero(ganadero);
+
+        }
+    }
+}
---
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Persistencia/AppRepositorios/IRepositorioGanadero.cs
@@ -0,0 +1,15 @@
+using Ganaderia.App.Dominio;
+
+namespace Ganaderia.App.Persistencia
+
+{
+public interface IRepositorioGanadero
+{
+void AddGanadero (Ganadero ganadero);
+
+}
+}
+
+
+
+
